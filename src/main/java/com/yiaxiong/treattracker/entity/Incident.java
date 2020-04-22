cb65135e406000efa61ee3b0cff053a2e233b10c
Yia Xiong <yxiong15@madisoncollege.edu>
2020-04-21 22:41:54 -0500

    update incident to include lombok

--- src/main/java/com/yiaxiong/treattracker/entity/Incident.java
+++ src/main/java/com/yiaxiong/treattracker/entity/Incident.java
@@ -37,47 +37,9 @@ public class Incident implements Serializable {
 
     private String description;
 
+    /**
+     * The empty constructor
+     */
     public Incident() {}
-/*
-    public Incident(LocalDateTime incidentDate, String description) {
-        this.incident_date = incidentDate;
-        this.description = description;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getIncident_date() {
-        return incident_date;
-    }
-
-    public void setIncident_date(LocalDateTime incident_date) {
-        this.incident_date = incident_date;
-    }
-
-    public User getUser() { return user; }
-
-    public void setUser(User user) { this.user = user; }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Resolution getResolution() {
-        return resolution;
-    }
-
-    public void setResolution(Resolution resolution) {
-        this.resolution = resolution;
-    }*/
 
 }
